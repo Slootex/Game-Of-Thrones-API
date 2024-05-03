0376e28ef836703cbc5cb5f255268e369a49f4f2
Slootex <lucasgloede20@gmail.com>
2024-05-03 14:02:09 +0200

    BookDetails

--- app/(drawer)/books/details.js
+++ app/(drawer)/books/details.js
@@ -1,79 +1,67 @@
-import { useLocalSearchParams } from 'expo-router';
-import { Text, TouchableOpacity, View } from 'react-native';
-import TopMenu from '../../../components/topMenu';
-import { SafeAreaView } from 'react-native-safe-area-context';
 import React, { useEffect, useRef, useState } from 'react';
+import { Text, TouchableOpacity, View } from 'react-native';
 import { FlatList } from 'react-native-gesture-handler';
-import { useCharactersCache } from '../../../constants';
+import { SafeAreaView } from 'react-native-safe-area-context';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useLocalSearchParams } from 'expo-router';
+
+import TopMenu from '../../../components/topMenu';
 import InfoCard from '../../../components/books/InfoCard';
 import SpecificInfo from '../../../components/books/SpecificInfo';
 import CharacterItem from '../../../components/books/CharacterItem';
 import AuthorCard from '../../../components/books/AuthorCard';
+import { getAgeFromBook } from '../../../utils/book';
+import { addCharacterToCaches, getCharacterFromAPI, getCharacterFromInMemoryCache, getCharacterFromInStorageCache, isCharacterInMemoryCache, isCharacterInStorageCache } from '../../../utils/character';
+
+const loadCharactersIndexSize = 3;
 
 /**
  * Renders the details of a book.
  * @returns {JSX.Element} The rendered details component.
  */
-export default function details() {
-
+export default function Details() {
     let { book } = useLocalSearchParams();
     book = JSON.parse(book);
-
-    const loadCharactersIndexSize = 3;
-
-    // Calculate the age of the book
-    const releasedDate = new Date(book.released);
-    const currentDate = new Date();
-    const ageInYears = currentDate.getFullYear() - releasedDate.getFullYear();
-
     const [charactersIndex, setCharactersIndex] = useState(0);
     const [charactersList, setCharacterList] = useState([]);
-    const [charactersCache, setCharactersCache] = useCharactersCache();
     const [isFetchingCharacters, setIsFetchingCharacters] = useState(false);
     const CharacterItemListRef = useRef();
-
-    const renderCharacterItem = ({ item }) => <CharacterItem item={item} />;
-
-  
+    
     /**
-     * Fetches three characters from the book's character list and updates the character list state.
-     * If a character is already cached, it retrieves the character data from the cache.
-     * If a character is not cached, it fetches the character data from the API and stores it in the cache.
-     * @returns {Promise<void>} A promise that resolves once the characters are fetched and updated.
+     * Fetches three characters from the book's character list.
+     * 
+     * @returns {Promise<void>} A promise that resolves when the characters are fetched.
      */
     const getThreeCharacters = async () => {
         if (isFetchingCharacters) {
-            return; // Exit the function if already fetching characters
+            return;
         }
         setIsFetchingCharacters(true);
 
-        let characters = book.characters.slice(charactersIndex, charactersIndex + loadCharactersIndexSize).filter(character => character.name != "");
+        const characters = book.characters
+            .slice(charactersIndex, charactersIndex + loadCharactersIndexSize)
+            .filter((character) => character.name !== '');
 
         for (const character of characters) {
-            if (charactersCache.find(char => char.url === character)) {
-                const characterData = charactersCache.find(char => char.url === character);
-
-                if (characterData.name != "") {
-                    setCharacterList(charactersList => [...charactersList, characterData]);
-                }
+            if(character == null) continue;
+            
+            if (isCharacterInMemoryCache(character)) {
+                const characterData = getCharacterFromInMemoryCache(character);
+                setCharacterList((charactersList) => [...charactersList, characterData]);
                 continue;
             } else {
-                let storage = await AsyncStorage.getItem(character);
-                storage = JSON.parse(storage);
-                
-                if (storage.name != "") {
-                    setCharacterList(charactersList => [...charactersList, storage]);
-                    setCharactersCache(charactersCache => [...charactersCache, storage]);
-                    continue;
+
+                if(await isCharacterInStorageCache(character)) {
+                    const storage = await getCharacterFromInStorageCache(character);
+
+                    setCharacterList((charactersList) => [...charactersList, storage]);
+                    addCharacterToCaches(character, storage);
                 } else {
                     const characterData = await getCharacterFromAPI(character);
+                    if(characterData.name == "") continue;
 
-                    if (characterData.name != "") {
-                        setCharacterList(charactersList => [...charactersList, characterData]);
-                        setCharactersCache(charactersCache => [...charactersCache, characterData]);
-                        AsyncStorage.setItem(character, JSON.stringify(characterData));
-                    }
+                    setCharacterList((charactersList) => [...charactersList, characterData]);
+                    addCharacterToCaches(character, characterData);
                 }
             }
         }
@@ -86,20 +74,22 @@ export default function details() {
         getThreeCharacters();
     }, []);
 
- 
+    const renderCharacterItem = ({ item }) => <CharacterItem item={item} />;
+    const ageInYears = getAgeFromBook(book);
+
     return (
         <SafeAreaView className="w-full h-full bg-zinc-800">
             <TopMenu />
             <View className="px-4 py-4">
-                <AuthorCard book={ book } />
+                <AuthorCard book={book} />
 
                 <View className="w-full py-2 mt-4">
                     <View className="flex flex-row content-evenly justify-between">
-                        <InfoCard name="Characters" text={ book.characters.length } />
+                        <InfoCard name="Characters" text={book.characters.length} />
 
-                        <InfoCard name="Pages" text={ book.numberOfPages } />
+                        <InfoCard name="Pages" text={book.numberOfPages} />
 
-                        <InfoCard name="Age" text={ ageInYears + " Yrs." } />
+                        <InfoCard name="Age" text={`${ageInYears} Yrs.`} />
                     </View>
                 </View>
 
@@ -107,13 +97,13 @@ export default function details() {
                     <View className="bg-zinc-700 rounded-lg w-full px-4 py-2">
                         <Text className="text-white text-xl font-medium">Specifics</Text>
 
-                        <SpecificInfo name="Publisher" text={ book.publisher } />
+                        <SpecificInfo name="Publisher" text={book.publisher} />
 
-                        <SpecificInfo name="Country" text={ book.country } />
+                        <SpecificInfo name="Country" text={book.country} />
 
-                        <SpecificInfo name="Media Type" text={ book.mediaType } />
+                        <SpecificInfo name="Media Type" text={book.mediaType} />
 
-                        <SpecificInfo name="ISBN" text={ book.isbn } />
+                        <SpecificInfo name="ISBN" text={book.isbn} />
                     </View>
                 </View>
 
@@ -125,38 +115,21 @@ export default function details() {
                             ref={CharacterItemListRef}
                             data={charactersList}
                             renderItem={renderCharacterItem}
-                            keyExtractor={item => item.url}
+                            keyExtractor={(item) => item.url}
                         />
 
-                        <TouchableOpacity 
-                            className="w-full py-2 mt-4 bg-zinc-600 rounded-lg"
-                            onPress={getThreeCharacters}
-                        >
+                        <TouchableOpacity className="w-full py-2 mt-4 bg-zinc-600 rounded-lg" >
                             {book.characters.length === 0 ? (
                                 <Text className="text-white text-center font-medium">No Characters available</Text>
                             ) : (
-                                <Text className="text-white text-center font-medium">View { loadCharactersIndexSize } More ({ charactersList.length }/{ book.characters.length })</Text>
+                                <Text className="text-white text-center font-medium">
+                                    View {loadCharactersIndexSize} More ({charactersList.length}/{book.characters.length})
+                                </Text>
                             )}
                         </TouchableOpacity>
                     </View>
                 </View>
             </View>
         </SafeAreaView>
-    )
+    );
 }
-
-/**
- * Fetches a character from the API.
- * @param {string} url - The URL of the API endpoint.
- * @returns {Promise<Object|Error>} - A promise that resolves to the character object or rejects with an error.
- */
-async function getCharacterFromAPI(url) {
-    try {
-        const response      = await fetch(url);
-        const responseJson  = await response.json();
-        
-        return responseJson;
-    } catch (error) {
-        return error;
-    }
-}  
